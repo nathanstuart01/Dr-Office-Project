17f4b7bb834a347fcbc63f1e60af065f31ee23f0
Nathan Stuart <nathan.stuart01@gmail.com>
2017-04-25 14:25:54 -0600

    seeded database, fixed button display when showing dr appointments and patient appointments, added view scheduled appointments to navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Doctor.create([
+  { id: '1', first_name: Faker::Name.first_name, last_name: Faker::Name.last_name},
+  { id: '2', first_name: Faker::Name.first_name, last_name: Faker::Name.last_name},
+  { id: '3', first_name: Faker::Name.first_name, last_name: Faker::Name.last_name},
+  { id: '4', first_name: Faker::Name.first_name, last_name: Faker::Name.last_name},
+  { id: '5', first_name: Faker::Name.first_name, last_name: Faker::Name.last_name}
+])
+
+Patient.create([
+  { id: '1', first_name: Faker::Name.first_name, last_name: Faker::Name.last_name},
+  { id: '2', first_name: Faker::Name.first_name, last_name: Faker::Name.last_name},
+  { id: '3', first_name: Faker::Name.first_name, last_name: Faker::Name.last_name},
+  { id: '4', first_name: Faker::Name.first_name, last_name: Faker::Name.last_name},
+  { id: '5', first_name: Faker::Name.first_name, last_name: Faker::Name.last_name}
+])
+
+Appointment.create([
+  { id: '1', doctor_id: Faker::Number.between(1, 5), patient_id: Faker::Number.between(1, 5), appointment_date: Faker::Date.forward(23), appointment_time: Faker::Time.between(DateTime.now - 1, DateTime.now)},
+  { id: '2', doctor_id: Faker::Number.between(1, 5), patient_id: Faker::Number.between(1, 5), appointment_date: Faker::Date.forward(23), appointment_time: Faker::Time.between(DateTime.now - 1, DateTime.now)},
+  { id: '3', doctor_id: Faker::Number.between(1, 5), patient_id: Faker::Number.between(1, 5), appointment_date: Faker::Date.forward(23), appointment_time: Faker::Time.between(DateTime.now - 1, DateTime.now)},
+  { id: '4', doctor_id: Faker::Number.between(1, 5), patient_id: Faker::Number.between(1, 5), appointment_date: Faker::Date.forward(23), appointment_time: Faker::Time.between(DateTime.now - 1, DateTime.now)},
+  { id: '5', doctor_id: Faker::Number.between(1, 5), patient_id: Faker::Number.between(1, 5), appointment_date: Faker::Date.forward(23), appointment_time: Faker::Time.between(DateTime.now - 1, DateTime.now)}
+  ])
+
+  puts "Dr. Pepper's Office Now Seeded"
